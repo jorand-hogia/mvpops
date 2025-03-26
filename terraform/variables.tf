1f10eddb6c1a05c6370fe5529dacd07ed04c6ada
JorgenA <jorgen.andersson@hogia.se>
2025-03-26 16:42:23 +0100

    Fix duplicate common_tags definition and ensure production resource group has team tag

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,43 +1,35 @@
 variable "prefix" {
-  description = "The prefix to use for all resources"
+  description = "Prefix to use for resource names"
   type        = string
   default     = "mvpops"
 }
 
 variable "environment" {
-  description = "The environment (prod, dev, etc.)"
+  description = "Environment (dev, test, prod)"
   type        = string
-  default     = "production"
+  default     = "prod"
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group"
+  description = "Name of the resource group"
   type        = string
   default     = "mvpops-production-rg"
 }
 
 variable "location" {
-  description = "The Azure region to deploy to"
+  description = "Azure region to deploy resources"
   type        = string
   default     = "eastus2"
 }
 
 variable "tenant_id" {
-  description = "The Azure AD tenant ID"
+  description = "Azure AD tenant ID"
   type        = string
+  default     = "00000000-0000-0000-0000-000000000000"
 }
 
 variable "alert_email" {
   description = "Email address for alerts"
   type        = string
   default     = "devops@example.com"
-}
-
-locals {
-  common_tags = {
-    Environment = var.environment
-    Project     = "MVPDevOps"
-    ManagedBy   = "Terraform"
-    Owner       = "DevOps Team"
-  }
 } 
